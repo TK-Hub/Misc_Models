bc011598a65fa9ac97c00b7c7f134f4abefdedfc
TK-Hub <54644447+TK-Hub@users.noreply.github.com>
2020-07-15 16:14:52 -0700

    Update MNIST.py

--- 01_MNIST/MNIST.py
+++ 01_MNIST/MNIST.py
@@ -1,6 +1,6 @@
 import keras
 from keras.datasets import mnist
-from keras.layers import Sequential
+from keras.models import Sequential
 from keras.layers import Dense
 from keras.optimizers import SGD
 from matplotlib import pyplot as plt
@@ -15,3 +15,13 @@ for k in range(12):
 plt.tight_layout()
 plt.show()
 
+# Reshaping the data to a one dimensional array of 784 numbers to fit the input layer geometry.
+x_train = x_train.reshape(60000, 784).astype('float32')
+x_valid = x_valid.reshape(60000, 784).astype('float32')
+
+x_train /= 255
+x_valid /= 255
+
+# Done formatting the model inputs, now the labels y are converted from int to one-hot encodings.
+n_classes = 10
+y_train = keras.utils
